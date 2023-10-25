5dacd794d81758c06e3d8dca07cb50da6b81fad6
Francesco Rapetti <rapettif80@yahoo.it>
2023-10-25 14:51:13 +0200

    Add field check to input data

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
 let control;
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let firstName;
+let surname;
+let color;
 
-const name = prompt("Inserisci il tuo nome");
-const surname = prompt("Inserisci il tuo cognome");
-let color = prompt("Inserisci il tuo colore preferito");
+firstName = promptManager(firstName, 'nome');
+surname = promptManager(surname, 'cognome');
+color = promptManager(color, 'colore')
 
-let password = `La tua password è: ${name}${surname}${color}#${Math.floor(Math.random() * 100)}`;
+function promptManager(input, inputName) {
+    while (!control) {
+        input = prompt(`Inserisci il tuo ${inputName}`);
+        if (input == null || input == '') {
+            alert(`Non hai inserito un ${inputName}`)
+        }
+    
+        else if (!stringController(input)) {
+            alert(`Non sono ammessi numeri nel ${inputName}`)
+        }
+        
+        else {
+            return input;
+        }
+    }
+}
 
-console.log(password);
+function stringController(stringa) {
+    for (let index = 0; index < stringa.length; index++) {
+        if (stringa[index] in numbers) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+let password = `La tua password è: ${firstName}${surname}${color}#${Math.floor(Math.random() * 100)}`;
+
+// console.log(password);
+document.getElementById("title").innerHTML = ''+password;
